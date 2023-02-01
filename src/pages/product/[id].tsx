4f7074dddae369a189f9ef411238ee35f6797967
HTM1000 <ID1355342@outlook.com>
2023-02-01 18:16:17 -0300

    e-commerce next

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,97 @@
+import { stripe } from '@/lib/stripe';
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
+import axios from 'axios';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+import Stripe from 'stripe';
+
+interface ProductIdProps {
+	product: {
+		id: string;
+		name: string;
+		imageUrl: string;
+		price: string;
+		description: string;
+		defaultPriceId: string;
+	};
+}
+
+export default function ProductId({ product }: ProductIdProps) {
+	const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+
+	const handler = async () => {
+		try {
+			setIsCreatingCheckoutSession(false);
+			const response = await axios.post('/api/checkout', {
+				priceId: product.defaultPriceId,
+			});
+
+			const { checkoutUrl } = response.data;
+
+			window.location.href = checkoutUrl;
+		} catch (e) {
+			setIsCreatingCheckoutSession(false)
+			alert('Falha de conexão com sistema de pagamento');
+		}
+	};
+
+	return (
+		<>
+			<Head>
+				<title>{product.name} | Ignite Shop</title>
+			</Head>
+			<ProductContainer>
+				<ImageContainer>
+					<Image src={product.imageUrl} width={520} height={480} alt='Produto da loja - Camiseta' />
+				</ImageContainer>
+
+				<ProductDetails>
+					<h1>{product.name}</h1>
+					<span>{product.price}</span>
+
+					<p>{product.description}</p>
+
+					<button disabled={isCreatingCheckoutSession} onClick={handler}>
+						Comprar
+					</button>
+				</ProductDetails>
+			</ProductContainer>
+		</>
+	);
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	return {
+		paths: [{ params: { id: 'prod_NEIAyx6ejWhPaf' } }],
+		fallback: true,
+	};
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+	const productId = params!.id;
+
+	const product = await stripe.products.retrieve(productId, {
+		expand: ['default_price'],
+	});
+
+	const price = product.default_price as Stripe.Price;
+
+	return {
+		props: {
+			product: {
+				id: product.id,
+				name: product.name,
+				imageUrl: product.images[0],
+				price: new Intl.NumberFormat('pt-BR', {
+					style: 'currency',
+					currency: 'BRL',
+				}).format(price.unit_amount! / 100),
+				description: product.description,
+				defaultPriceId: price.id,
+			},
+		},
+		revalidate: 60 * 60 * 1,
+	};
+};
